58ed01a4b9dc482892d9bc02c86b032892f8fb11
mitaksim1 <miriamt.simonnet@gmail.com>
2020-06-15 17:56:14 +0200

    Ajout fichiers de styles, affichage du logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 /**
  * Import des composants
  */
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
-  // const counter = useState(0);
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    // On attribue une nouvelle valeur à counter
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title="Hello World"/>
-
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Plus 1</button>
-    </div>
+      <Home />
   );
 }
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import logo from '../../assets/logo.svg';
+
+const Home = () => {
+    return (
+        <div id="page-home">
+            <div className="content">
+                <img src={logo} alt="Ecollect" />
+            </div>
+        </div>
+    )
+}
+
+export default Home;

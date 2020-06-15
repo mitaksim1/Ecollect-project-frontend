90e3ae3b95242ab5c1dc107dfea986afcf0b4dfb
mitaksim1 <miriamt.simonnet@gmail.com>
2020-06-15 14:02:07 +0200

    Exemple utilisation de state, useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 /**
@@ -7,11 +7,20 @@ import './App.css';
 import Header from './Header';
 
 function App() {
+  // const counter = useState(0);
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    // On attribue une nouvelle valeur à counter
+    setCounter(counter + 1);
+  }
+
   return (
     <div>
       <Header title="Hello World"/>
 
-      <h1>Contenu de l'application</h1>
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Plus 1</button>
     </div>
   );
 }

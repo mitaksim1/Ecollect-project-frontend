8fbbd3f11a289491692ccb6b2766a15305a66d90
mitaksim1 <miriamt.simonnet@gmail.com>
2020-06-15 11:41:35 +0200

    Passage des propriétés d'un composant à un autre

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
+/**
+ * Import des composants
+ */
+import Header from './Header';
+
 function App() {
   return (
-    <h1>Hello World</h1>  
-    
+    <div>
+      <Header title="Hello World"/>
+
+      <h1>Contenu de l'application</h1>
+    </div>
   );
 }
 
